8cb0f2a89c803a9ffc763e3ff9485e871bc5661a
Elijah Kin <elijah.m.kin@gmail.com>
2024-09-11 23:18:00 -0400

    Formatting

--- assignment-1/problem2.cpp
+++ assignment-1/problem2.cpp
@@ -1,8 +1,8 @@
-#include <stdlib.h>
 #include <cmath>
-#include <vector>
 #include <cstdio>
 #include <iostream>
+#include <stdlib.h>
+#include <vector>
 
 /* Count the number of edges in the directed graph defined by the adjacency
    matrix A. A is an NxN adjacency matrix stored in row-major. A represents a
@@ -12,50 +12,50 @@
       input: [[0, 0, 0, 1], [0, 0, 0, 1], [0, 0, 0, 1], [1, 1, 1, 0]]
       output: 3
 */
-int edgeCount(std::vector<int> const& A, size_t N) {
-    int count = 0;
+int edgeCount(std::vector<int> const &A, size_t N) {
+  int count = 0;
 
-    for (size_t i = 0; i < N; i += 1) {
-        for (size_t j = 0; j < N; j += 1) {
-            if (A[i * N + j] == 1) {
-                count++;
-            }
-        }
+  for (size_t i = 0; i < N; i += 1) {
+    for (size_t j = 0; j < N; j += 1) {
+      if (A[i * N + j] == 1) {
+        count++;
+      }
     }
+  }
 
-    return count;
+  return count;
 }
 
 int main(int argc, char **argv) {
-    int N = 128;
-    int seed = 17;
+  int N = 128;
+  int seed = 17;
 
-    if (argc == 2) {
-        N = std::stoi(argv[1]);
+  if (argc == 2) {
+    N = std::stoi(argv[1]);
+  }
+  if (argc == 3) {
+    N = std::stoi(argv[1]);
+    seed = std::stoi(argv[2]);
+  }
+
+  std::vector<int> A(N * N);
+  srand(seed);
+
+  std::fill(A.begin(), A.end(), 0);
+  for (int i = 0; i < N; i += 1) {
+    for (int j = 0; j < N; j += 1) {
+      if (rand() % 2 == 0) {
+        A[i * N + j] = 1;
+      }
     }
-    if (argc == 3) {
-	    N = std::stoi(argv[1]);
-	    seed = std::stoi(argv[2]);
-    }
+  }
 
-    std::vector<int> A(N * N);
-    srand(seed);
+  // double totalTime = 0.0;
+  // double start = omp_get_wtime();
 
-    std::fill(A.begin(), A.end(), 0);
-    for (int i = 0; i < N; i += 1) {
-        for (int j = 0; j < N; j += 1) {
-            if (rand() % 2 == 0) {
-                A[i * N + j] = 1;
-            }
-        }
-    }
+  int count = edgeCount(A, N);
+  printf("Count : %d\n", count);
 
-    // double totalTime = 0.0;
-    // double start = omp_get_wtime();
-
-    int count = edgeCount(A, N);
-    printf("Count : %d\n", count);
-
-    // totalTime = omp_get_wtime() - start;
-    // printf("Time: %.5f\n", totalTime);
+  // totalTime = omp_get_wtime() - start;
+  // printf("Time: %.5f\n", totalTime);
 }
