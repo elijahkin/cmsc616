6dd407c5a49c250f21e2a4331c7aeef454b3bf14
Elijah Kin <elijah.m.kin@gmail.com>
2024-09-17 19:21:28 -0400

    Updating submit script

--- assignment-1/submit.sh
+++ assignment-1/submit.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Request 8 cores on a single node for 5 minutes
+# Request 64 cores on a single node for 5 minutes
 #SBATCH -N 1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=64
 #SBATCH -t 00:05:00
 #SBATCH -A cmsc416-class
 #SBATCH --mem-bind=local
-
+#SBATCH --exclusive
 
 # This is to suppress the warning about not finding a GPU resource
 export OMPI_MCA_mpi_cuda_support=0
@@ -14,15 +14,16 @@ export OMPI_MCA_mpi_cuda_support=0
 # Env variable to reduce performance variability
 export OMP_PROCESSOR_BIND=true
 
-# Set the number of OpenMP threads to 8
-export OMP_NUM_THREADS=8
+declare -a nums_threads=(1 2 4 8 16 32 64)
 
+for n in "${nums_threads[@]}"
+do
+  # Set the number of OpenMP threads to n
+  export OMP_NUM_THREADS=n
 
-# Run the executable
-./problem1 &> problem1.out
-./problem2 &> problem2.out
-./problem3 &> problem3.out
-./problem4 &> problem4.out
-./problem1_serial &> problem1_serial.out
-./problem2_serial &> problem2_serial.out
-./problem3_serial &> problem3_serial.out
+  # Run the executables
+  ./problem1 16384 &> problem1_$n.out
+  ./problem2 8192 &> problem2_$n.out
+  ./problem3 67108864 &> problem3_$n.out
+  ./problem4 8192 &> problem4_$n.out
+done
